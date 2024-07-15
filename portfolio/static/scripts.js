b7ad053b2f63b7dd0ea45c9c69e70f04360de377
kshitijakarsh <kshitijakarsh@gmail.com>
2024-07-15 09:52:37 +0530

    another one

---
+++ portfolio/static/scripts.js
@@ -0,0 +1,8 @@
+function openCard(imageSrc) {
+    const largeCard = document.getElementById("largeCard");
+    const largeCardImage = document.getElementById("largeCardImage");
+  
+    largeCard.style.display = "block";
+    largeCardImage.src = imageSrc;
+  }
+  
